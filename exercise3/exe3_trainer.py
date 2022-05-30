2555145efcfb052c2d7cd585fefdf0b4b1330a9a
Leon Hafner <hafnerl@cip.ifi.lmu.de>
2022-05-30 12:32:01 +0200

    trained model after hypopt

--- exercise3/exe3_trainer.py
+++ exercise3/exe3_trainer.py
@@ -31,7 +31,85 @@ def collate_paired_sequences(data: List[Tuple[torch.Tensor, int]]) -> Tuple[torc
         torch.tensor([[x] for x in list(zip(*data))[1]]).float()
     )
 
+class Trainer:
+    def __init__(self, model: Any, device: torch.device, save_path: str = 'hypopt_model.pth'):
+        super(Trainer, self).__init__()
 
+        self.device = device
+        self.model = model.to(self.device)
+        self.save_path = save_path
+
+        self.sigmoid = nn.Sigmoid()
+        self.bce_with_logits = nn.BCEWithLogitsLoss()
+        self.bce = nn.BCELoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+
+    def train(self, dataloader, num_epoch: int):
+        self.model.train()
+        for epoch in tqdm(range(num_epoch), desc='Train Epoch', position=0, leave=True, ascii=True):
+            running_loss, num_seqs = 0.0, 0
+            for i, data in enumerate(tqdm(dataloader, desc='Epoch Progress', position=0, leave=True, ascii=True), 0):
+                inputs, labels = data
+                labels = labels.view(-1, 1).to(self.device)
+
+                self.optimizer.zero_grad()
+
+                outputs = self.model(inputs)
+
+                loss = self.bce_with_logits(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+                num_seqs += len(labels)
+
+                running_loss += loss.item()
+            print(f'[{epoch + 1}] loss: {running_loss / num_seqs:.3f}')
+
+        print('Finished Training')
+        self.save_model()
+
+    def validate(self, val_dataloader):
+        self.optimizer.zero_grad()
+        self.model.eval()
+        running_loss, num_seqs = 0.0, 0
+        prediction = None
+        allLabels = None
+        for i, data in enumerate(tqdm(val_dataloader, desc='Validation Progress', position=0, leave=True, ascii=True), 0):
+            inputs, labels = data
+            labels = labels.view(-1, 1).to(self.device)
+
+            outputs = self.model.predict(inputs)
+
+            loss = self.bce(outputs, labels)
+
+            num_seqs += len(labels)
+            running_loss += loss.item()
+
+            if prediction is None:
+                prediction = self.model.predict(inputs)
+                allLabels = labels
+            else:
+                prediction = torch.cat((prediction, self.model.predict(inputs)), 0)
+                allLabels = torch.cat((allLabels, labels), 0)
+
+
+        print("Validation loss: "+str(running_loss/num_seqs))
+        prediction = prediction.detach()
+        prediction = prediction >= 0.5
+        allLabels = allLabels.detach()
+        allLabels = allLabels >= 0.5
+        print("precision: " + str(met.precision_score(allLabels, prediction)))
+        print("recall: " + str(met.recall_score(allLabels, prediction)))
+        print("accuracy: " + str(met.accuracy_score(allLabels, prediction)))
+        print("F1-score: " + str(met.f1_score(allLabels, prediction)))
+        print("MCC: " + str(met.matthews_corrcoef(allLabels, prediction)))
+        return running_loss / num_seqs
+
+
+    def save_model(self):
+        if self.save_path:
+            torch.save(
+                {'model': self.model.state_dict(), 'config': self.model.get_config()}, self.save_path
+            )
 
 def getDataloaders():
     h5file = h5py.File('data/embedding.h5', 'r')
@@ -83,7 +161,7 @@ def objective(trial):
     bce_with_logits = nn.BCEWithLogitsLoss()
     bce = nn.BCELoss()
 
-    train_dataloader, val_dataloader = getDataloaders()
+    val_dataloader, train_dataloader = getDataloaders()
 
     optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
     lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
@@ -181,6 +259,18 @@ if __name__ == '__main__':
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
 
+
+    test_dataloader, train_dataloader = getDataloaders()
+    
     model = Model(trial)
-    save_model("hypopt_model.pth", model)
+    trainer = Trainer(model, torch.device('cpu'))
+    trainer.train(train_dataloader, 20)
+
+    checkpoint = torch.load('hypopt_model.pth')
+    model = Model(**checkpoint['config'])
+    model.load_state_dict(checkpoint['model'])
+    model.eval()
+
+    trainer = Trainer(model, device=torch.device('cpu'))
+    trainer.validate(test_dataloader)
 
