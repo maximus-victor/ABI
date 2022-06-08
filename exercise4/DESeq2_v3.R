8b35311bd222914f61cb25e988178112d919ef75
harl <64c1d99f68f25464>
2022-06-08 12:41:35 +0200

    ex4_3 init

---
+++ exercise4/DESeq2_v3.R
@@ -0,0 +1,38 @@
+################################################################################
+# Authors:  Markus List, Alexander Dietrich
+# Created:  11.10.2017
+# Updated:  29.05.2022
+# Purpose:  Analyse RNAseq data from overexpressing HepG2 cells (OE vs CT)
+################################################################################
+
+
+### DEPENDENCIES ###
+
+library(DESeq2)
+library(tximport)
+library(tximportData)
+library(biomaRt)
+
+### DESeq2 ###
+quant_dir <- "." # adjust --> parent directory in which the result folders of kallisto are stored
+samples <- list.files(quant_dir)
+
+samples <- data.frame(project = "HepG2", 
+                      sample = samples, 
+                      condition = c(rep("CT", 3), rep("OE", 3)))
+
+
+quant_files <- paste0(quant_dir, samples$sample, "/abundance.h5")
+names(quant_files) <- samples$sample
+
+dir <- system.file("extdata", package="tximportData")
+tx2gene <- read.csv(file.path(dir, "tx2gene.gencode.v27.csv"))
+
+txi <- tximport(quant_files, type = "kallisto", tx2gene = tx2gene)
+
+# create DESeq2 object
+dds <- DESeqDataSetFromTximport(txi, colData = samples, design = ~ condition)
+dds <- dds[ rowSums(counts(dds)) > 1, ]
+dds <- DESeq(dds)
+
+#continue here...
